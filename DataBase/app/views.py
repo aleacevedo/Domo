976703bde5274e5fd8469b69b3b60df58b0c561c
Alejo <ale.acevedo@live.com.ar>
2017-05-18 16:41:11 -0300

    Edito DataBase

--- DataBase/app/views.py
+++ DataBase/app/views.py
@@ -1,32 +1,43 @@
-from flask  import jsonify, abort, request, g
+from flask import jsonify, abort, g
 from flask.ext.httpauth import HTTPBasicAuth
 from app import app, db, models
 
 auth = HTTPBasicAuth()
 
+
+def model_to_dict(model):
+    dict = {}
+    for x in model:
+        x = x.__dict__
+        del x['_sa_instance_state']
+        dict[x['nickName']] = x
+    return dict
+
+
 @app.route('/Data/api/v1.0/Users')
 @auth.login_required
 def get_Users():
-    users = {}
     allUsers = models.User.query.all()
-    for user in allUsers:
-        users[allUsers['eMail']] = user
-    return jsonify(users)
+    return jsonify(model_to_dict(allUsers))
+
 
 @app.route('/Data/api/v1.0/User/<id>')
 @auth.login_required
 def get_User(id):
-    user = None
-    #user = model.User.query.filter_by(eMail = id or nickName = id).first()
-    if(not user):
+    user = models.User.query.filter_by(email=id).first()
+    if not user:
+        user = models.User.query.filter_by(nickName=id).first()
+    if not user:
         abort(500)
     return jsonify(user)
 
+
 @auth.verify_password
 def verify_password(id, password):
-    user = None
-    #user = models.User.query.filter_by(eMail = id or nickName = id).first
-    if(not user or not user.verify_password(password)):
+    user = models.User.query.filter_by(email=id).first()
+    if not user:
+        user = models.User.query.filter_by(nickName=id).first()
+    if not user or not user.verify_password(password):
         return False
     g.user = user
     return True
