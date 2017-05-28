97fdb5d60b35cb227a5458a119cf4862731536ad
Alejo <ale.acevedo@live.com.ar>
2017-05-28 19:49:58 -0300

    Agrego las pruebas

--- DataBase/app/__init__.py
+++ DataBase/app/__init__.py
@@ -3,8 +3,6 @@ from config import basedir
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
---
+++ Pruebas/DataBaseTest.py
@@ -0,0 +1,28 @@
+import unittest, requests, sys, os
+
+AUTH = ("admin", "admin")
+BASEURL = "http://localhost:5555/Data/api/v1.0"
+
+
+class DataBaseTest(unittest.TestCase):
+
+    def test_emptyDataBase(self):
+        """ La base de datos se crea siempre con el usuario admin"""
+        url = BASEURL + "/Users"
+        r = requests.get(url, auth=AUTH)
+        self.assertEqual(len(r.json()), 1, "La base de datos no esta vacia")
+
+    def test_addUserDataBase(self):
+        """ Agrego usuario, debe responder con status code 200"""
+        url = BASEURL + "/User"
+        nwuser = ("Alejo", "19051996")
+        payload1 = {'email':"ale.acevedo@live.com.ar", 'nickName':"Alejo", 'password':"19051996"}
+        payload2 = {'email': "fede.cava@live.com.ar", 'nickName': "Federico", 'password': "20091995"}
+        r = requests.post(url, json=payload1, auth=AUTH)
+        self.assertEquals(r.status_code, 200, "El usuario 1 no pudo ser agregado")
+        r = r = requests.post(url, json=payload2, auth=nwuser)
+        self.assertEquals(r.status_code, 200, "El usuario 2 no pudo ser agregado, revise el usuario 1")
+
+
+if __name__ == '__main__':
+    unittest.main()
